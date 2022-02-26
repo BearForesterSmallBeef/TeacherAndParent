e20729ec7b5a114e05dc494a52ecbfd846ac2412
TheMerret <merret@bk.ru>
2022-02-26 19:52:16 +0300

    render templates with bootstrap flask module

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+
+
+bootstrap = Bootstrap5()
 
 
 def create_app(config_type):
     app = Flask(__name__)
     app.config.from_object(config_type)
+
+    bootstrap.init_app(app)
 
     from .views.main import main
     from .views.auth import auth
