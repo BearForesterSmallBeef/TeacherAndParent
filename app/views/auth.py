d152c7b9375b4e6e3f745f1fc98864ef9ec557d2
TheMerret <merret@bk.ru>
2022-02-26 23:42:22 +0300

    Separate registration form for parent and teacher

--- app/views/auth.py
+++ app/views/auth.py
@@ -18,10 +18,49 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Зарегистрироваться")
 
 
+class RegisterTypeForm(FlaskForm):
+    user_status = SelectField("Статус", choices=[("parent", "Родитель"), ("teacher", "Учитель")])
+    submit = SubmitField("Далее")
+
+
+class RegisterParentForm(FlaskForm):
+    full_name = StringField("ФИО", validators=[DataRequired()])
+    school = StringField("Школа", validators=[DataRequired()])
+    submit = SubmitField("Зарегистрироваться")
+
+
+class RegisterTeacherForm(FlaskForm):
+    full_name = StringField("ФИО", validators=[DataRequired()])
+    forms = SelectField("Классы", choices=list(zip(map(lambda x: f"form{x}", range(1, 12)),
+                                                   range(1, 12))))
+    subjects = SelectField("Предметы", choices=[("math", "Математика"), ("physics", "Физика"),
+                                                ("it", "Информатика")])
+    submit = SubmitField("Зарегистрироваться")
+
+
 @auth.route("/signup", methods=["GET", 'POST'])
-def signup():
-    register_form = RegisterForm()
+def signup_user_status():
+    register_form = RegisterTypeForm()
     if register_form.validate_on_submit():
-        flash("Вы успешно зарегистрировались", category="success")
+        user_status = register_form.user_status.data
+        return redirect(f"/signup/{user_status}")
+    return render_template("auth/register.html", form=register_form,
+                           header="Регистрация. Тип пользователя.")
+
+
+@auth.route("/signup/parent", methods=["GET", "POST"])
+def register_parent():
+    register_form = RegisterParentForm()
+    if register_form.validate_on_submit():
         return redirect("/login")
-    return render_template("auth/register.html", form=register_form)
+    return render_template("auth/register.html", form=register_form,
+                           header="Регистрация. Родитель.")
+
+
+@auth.route("/signup/teacher", methods=["GET", "POST"])
+def register_teacher():
+    register_form = RegisterTeacherForm()
+    if register_form.validate_on_submit():
+        return redirect("/login")
+    return render_template("auth/register.html", form=register_form,
+                           header="Регистрация. Учитель.")
