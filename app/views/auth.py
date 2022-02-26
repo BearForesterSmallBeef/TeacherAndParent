ab50094d68a6647d6ec09d735a0e496df8f368a4
TheMerret <merret@bk.ru>
2022-02-26 23:43:57 +0300

    remove common registration form

--- app/views/auth.py
+++ app/views/auth.py
@@ -6,18 +6,6 @@ from wtforms.validators import DataRequired
 auth = Blueprint("auth", __name__)
 
 
-class RegisterForm(FlaskForm):
-    full_name = StringField("ФИО", validators=[DataRequired()])
-    school = StringField("Школа", validators=[DataRequired()])
-    status = SelectField("Статус", choices=[("parent", "Родитель"), ("teacher", "Учитель")],
-                         default="parent")
-    forms = SelectField("Классы", choices=list(zip(map(lambda x: f"form{x}", range(1, 12)),
-                                                   range(1, 12))))
-    subjects = SelectField("Предметы", choices=[("math", "Математика"), ("physics", "Физика"),
-                                                ("it", "Информатика")])
-    submit = SubmitField("Зарегистрироваться")
-
-
 class RegisterTypeForm(FlaskForm):
     user_status = SelectField("Статус", choices=[("parent", "Родитель"), ("teacher", "Учитель")])
     submit = SubmitField("Далее")
