00be360b48919f3ad1cea01d70cf3270e119cbf0
BearForesterSmallBeef <BearForest2006@gmail.com>
2022-04-02 22:32:48 +0300

    make parent registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, request
 
-from .forms import RegisterTypeForm, RegisterParentForm, RegisterTeacherForm
+from .forms import RegisterTypeForm, RegisterParentForm, RegisterTeacherForm, RegistrationParentForm, BackToParentReg
+from app import db
+from app.models import Class, User, Parent, Consultation, TeacherSubjectsClasses, Subject, RolesIds
+
 
 auth = Blueprint("auth", __name__)
 
@@ -31,3 +34,43 @@ def register_teacher():
         return redirect("/login")
     return render_template("auth/register.html", form=register_form,
                            header="Регистрация. Учитель.")
+
+
+def create_parent(login, password, name, surname, classes,  middle_name=""):
+    try:
+        db.session.add(
+            User(login=login, password=password,
+                 name=name,
+                 surname=surname, middle_name=middle_name,
+                 role_id=RolesIds.PARENT))
+        user_id = db.session.query(User).filter(User.login == login).first().id
+        class_id = db.session.query(Class).filter(Class.name == classes).first().id
+        db.session.add(
+            Parent(user_id=user_id, class_id=class_id))
+        db.session.commit()
+    except Exception as ex:
+        print(ex)
+        return 0
+    return 1
+
+
+@auth.route("/parent_registration", methods=['GET', 'POST'])
+def parent_registration():
+    form = RegistrationParentForm()
+    form.classes.choices = sorted([(i.name, i.name) for i in db.session.query(Class)],
+                                  key=lambda x: int(x[0].split("-")[0]))
+    if form.validate_on_submit():
+        flag = create_parent(request.form["login"], request.form["password"], request.form["username"],
+                             request.form["usersurename"], request.form["classes"],
+                             middle_name=str(request.form["usermiddlename"]))
+        return redirect(f"/reg_result/{flag}")
+    return render_template("auth/parent_reg.html", form=form)
+
+
+@auth.route("/reg_result/<ok>", methods=['GET', 'POST'])
+def reg_result(ok):
+    form = BackToParentReg()
+    flag = bool(int(ok))
+    if form.validate_on_submit():
+        return redirect("/parent_registration")
+    return render_template("auth/reg_result.html", form=form, flag=flag)
