a2779f1841a31e6a9bdcf039fb0595fe1641cd33
BearForesterSmallBeef <BearForest2006@gmail.com>
2022-04-05 19:59:02 +0300

    make login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request
+from flask import Blueprint, redirect, render_template, request, flash
 
 from app import db
 from app.models import Class, User, Parent, RolesIds
@@ -16,24 +16,6 @@ def signup_user_status():
         return redirect(f"/signup/{user_status}")
     return render_template("auth/register.html", form=register_form,
                            header="Регистрация. Тип пользователя.")
-
-
-@auth.route("/signup/parent", methods=["GET", "POST"])
-def register_parent():
-    register_form = RegisterParentForm()
-    if register_form.validate_on_submit():
-        return redirect("/login")
-    return render_template("auth/register.html", form=register_form,
-                           header="Регистрация. Родитель.")
-
-
-@auth.route("/signup/teacher", methods=["GET", "POST"])
-def register_teacher():
-    register_form = RegisterTeacherForm()
-    if register_form.validate_on_submit():
-        return redirect("/login")
-    return render_template("auth/register.html", form=register_form,
-                           header="Регистрация. Учитель.")
 
 
 def create_parent(login, password, name, surname, classes, middle_name=""):
@@ -54,7 +36,7 @@ def create_parent(login, password, name, surname, classes, middle_name=""):
     return 1
 
 
-@auth.route("/parent_registration", methods=['GET', 'POST'])
+@auth.route("/signup/parent", methods=['GET', 'POST'])
 def parent_registration():
     form = RegistrationParentForm()
     form.classes.choices = sorted([(i.name, i.name) for i in db.session.query(Class)],
@@ -64,19 +46,24 @@ def parent_registration():
                              request.form["username"],
                              request.form["usersurename"], request.form["classes"],
                              middle_name=str(request.form["usermiddlename"]))
-        return redirect(f"/reg_result/{flag}")
+        flag = bool(int(flag))
+        if flag:
+            flash("Учетная запись для родителя успешна создана", category="success")
+        else:
+            flash("ПРОИЗОШЕЛ СБОЙ, пожалуйста, повторите попытку позже", category="error")
+        return redirect(f"/")
     return render_template("auth/auth.html", form=form)
 
 
-@auth.route("/reg_result/<ok>", methods=['GET', 'POST'])
+@auth.route("/signup/parent/reg_result/<ok>", methods=['GET', 'POST'])
 def reg_result(ok):
     flag = bool(int(ok))
     if request.form.get('Назад') == 'Назад':
-        return redirect('/parent_registration')
+        return redirect('/signup/parent')
     return render_template("auth/reg_result.html", flag=flag)
 
 
-@auth.route("/user_login", methods=['GET', 'POST'])
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     text = "Вход в учетную запись"
