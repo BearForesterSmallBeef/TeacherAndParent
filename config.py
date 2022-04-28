3d31c552c60d5ec65a2d1720d5b416ddad247bfc
TheMerret <merret@bk.ru>
2022-04-28 20:22:04 +0300

    fix production sqlalchemy db url

--- config.py
+++ config.py
@@ -20,7 +20,8 @@ class Config:
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "").replace("postgres://",
+                                                                    "postgresql://", 1)
 
 
 class DevelopmentConfig(Config):
